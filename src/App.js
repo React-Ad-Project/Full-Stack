220c3e6067de5bc944be5c65d477134fca1f5e02
abhinav-TB <abhinavtb@gmail.com>
2020-04-09 21:14:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./Components/Home";
+import navbar from "./Components/navbar";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-  }
+ 
 
   componentDidMount() {}
 
   render() {
     return (
+
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={Home} />
+          <Route path="/" component={navbar} />
         </Switch>
       </BrowserRouter>
     );
