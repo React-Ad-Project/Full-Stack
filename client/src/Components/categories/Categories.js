34459553574feb548e1a3fc7264e3112b296b4a8
abhinav-TB <abhinavtb@gmail.com>
2020-04-15 18:11:39 +0530

    adeed all images and set padding

--- client/src/Components/categories/Categories.js
+++ client/src/Components/categories/Categories.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import im from './catagory/1.png'
-import './categories.css'
+import i1 from './catagory/1.png'
+import i2 from './catagory/2.png'
+import i3 from './catagory/3.png'
+import i4 from './catagory/4.png'
+import i5 from './catagory/5.png'
+import i6 from './catagory/6.png'
+import i7 from './catagory/7.png'
+import i8 from './catagory/8.png'
+
 export default function Categories() {
     return (
     <div>
@@ -21,12 +28,11 @@ export default function Categories() {
                 <div className="col-xl-3 col-md-4 col-lg-3">
                     <div className="single_catagory">
                         <div className="thumb">
-                            <img src={im} alt="sfsd"/>
+                            <img src={i1} alt="sfsd"/>
                         </div>
                         <div className="hover_overlay">
                             <div className="hover_inner">
                                 <a href="Listings.html"><h4>Electronics and appliances</h4></a>
-                                <span>05 Listings</span>
                             </div>
                         </div>
                     </div>
@@ -34,12 +40,11 @@ export default function Categories() {
                 <div className="col-xl-3 col-md-4 col-lg-3">
                     <div className="single_catagory">
                         <div className="thumb">
-                            <img src="img/catagory/2.png" alt=""/>
+                            <img src={i2} alt=""/>
                         </div>
                         <div className="hover_overlay">
                             <div className="hover_inner">
                                 <a href="Listings.html"><h4>Movies, music and events</h4></a>
-                                <span>05 Listings</span>
                             </div>
                         </div>
                     </div>
@@ -47,12 +52,11 @@ export default function Categories() {
                 <div className="col-xl-3 col-md-4 col-lg-3">
                     <div className="single_catagory">
                         <div className="thumb">
-                            <img src="img/catagory/3.png" alt=""/>
+                            <img src={i3} alt=""/>
                         </div>
                         <div className="hover_overlay">
                             <div className="hover_inner">
                                     <a href="Listings.html"><h4>Tours and travel</h4></a>
-                                <span>05 Listings</span>
                             </div>
                         </div>
                     </div>
@@ -60,12 +64,11 @@ export default function Categories() {
                 <div className="col-xl-3 col-md-4 col-lg-3">
                     <div className="single_catagory">
                         <div className="thumb">
-                            <img src="img/catagory/4.png" alt=""/>
+                            <img src={i4} alt=""/>
                         </div>
                         <div className="hover_overlay">
                             <div className="hover_inner">
                                     <a href="Listings.html"><h4>Movies, music and events</h4></a>
-                                <span>05 Listings</span>
                             </div>
                         </div>
                     </div>
@@ -73,12 +76,11 @@ export default function Categories() {
                 <div className="col-xl-3 col-md-4 col-lg-3">
                     <div className="single_catagory">
                         <div className="thumb">
-                            <img src="img/catagory/5.png" alt=""/>
+                            <img src={i5} alt=""/>
                         </div>
                         <div className="hover_overlay">
                             <div className="hover_inner">
                                     <a href="Listings.html"><h4>Fashion and lifestyle</h4></a>
-                                <span>05 Listings</span>
                             </div>
                         </div>
                     </div>
@@ -86,12 +88,11 @@ export default function Categories() {
                 <div className="col-xl-3 col-md-4 col-lg-3">
                     <div className="single_catagory">
                         <div className="thumb">
-                            <img src="img/catagory/6.png" alt=""/>
+                            <img src={i6} alt=""/>
                         </div>
                         <div className="hover_overlay">
                             <div className="hover_inner">
                                     <a href="Listings.html"><h4>Home and furniture</h4></a>
-                                <span>05 Listings</span>
                             </div>
                         </div>
                     </div>
@@ -99,12 +100,11 @@ export default function Categories() {
                 <div className="col-xl-3 col-md-4 col-lg-3">
                     <div className="single_catagory">
                         <div className="thumb">
-                            <img src="img/catagory/7.png" alt=""/>
+                            <img src={i7} alt=""/>
                         </div>
                         <div className="hover_overlay">
                             <div className="hover_inner">
                                     <a href="Listings.html"><h4>Healthcare and nutrition </h4></a>
-                                <span>05 Listings</span>
                             </div>
                         </div>
                     </div>
@@ -112,12 +112,11 @@ export default function Categories() {
                 <div className="col-xl-3 col-md-4 col-lg-3">
                     <div className="single_catagory">
                         <div className="thumb">
-                            <img src="img/catagory/8.png" alt=""/>
+                            <img src={i8} alt=""/>
                         </div>
                         <div className="hover_overlay">
                             <div className="hover_inner">
                                     <a href="Listings.html"><h4>Food and drinks</h4></a>
-                                <span>05 Listings</span>
                             </div>
                         </div>
                     </div>
