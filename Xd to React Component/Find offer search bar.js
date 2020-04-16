3e7ebc742345d93c63cc33fdc737f0ff245b9562
Vasant Joseph <vasantjoseph@live.com>
2020-04-16 11:44:08 +0530

    Add files via upload

---
+++ Xd to React Component/Find offer search bar.js
@@ -0,0 +1,53 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+import Svg, { Defs, G, Rect, Path, Text, TSpan } from "react-native-svg";
+/* Adobe XD React Exporter has dropped some elements not supported by react-native-svg: style, filter */
+
+const FindOfferComponent = () => (
+  <Svg width={1287} height={451} viewBox="0 0 1287 451">
+    <Defs />
+    <G transform="translate(79.5 -7.5)">
+      <G className="g" transform="matrix(1, 0, 0, 1, -79.5, 7.5)">
+        <G className="a" transform="translate(148.5 138.5)">
+          <Rect className="e" width={990} height={154} rx={20} />
+          <Rect className="f" x={1} y={1} width={988} height={152} rx={19} />
+        </G>
+      </G>
+      <Path
+        className="b"
+        d="M59.87,60.407a2.831,2.831,0,0,1-4.056,0l-7.224-7.224a24.173,24.173,0,1,1,3.992-4.056l7.224,7.224A2.888,2.888,0,0,1,59.87,60.407ZM33.89,16.493a17.87,17.87,0,1,0,17.87,17.87A17.849,17.849,0,0,0,33.89,16.493Z"
+        transform="translate(113 187.4)"
+      />
+      <Text className="c" transform="translate(209 241)">
+        <TSpan x={0} y={0}>
+          {"Find Offers "}
+        </TSpan>
+      </Text>
+      <G transform="translate(955.626 196.235)">
+        <Path
+          className="d"
+          d="M14.853,29.984V2.023a2.033,2.033,0,0,0-4.065,0V29.984a7.879,7.879,0,0,0,0,15.248V51.5a2.033,2.033,0,0,0,4.065,0V45.231a7.88,7.88,0,0,0,0-15.248ZM12.82,41.462a3.854,3.854,0,1,1,3.854-3.852A3.858,3.858,0,0,1,12.82,41.462Z"
+          transform="translate(10.554 0.316)"
+        />
+        <Path
+          className="d"
+          d="M28.268,15.254A7.914,7.914,0,0,0,22.377,7.63V2.248a2.033,2.033,0,0,0-4.065,0V7.63a7.881,7.881,0,0,0,0,15.25V51.728a2.033,2.033,0,1,0,4.065,0V22.88A7.915,7.915,0,0,0,28.268,15.254ZM20.344,19.11A3.855,3.855,0,1,1,24.2,15.254,3.862,3.862,0,0,1,20.344,19.11Z"
+          transform="translate(21.109 0.631)"
+        />
+        <Path
+          className="d"
+          d="M7.329,7.182V1.8a2.033,2.033,0,1,0-4.065,0V7.182a7.879,7.879,0,0,0,0,15.248V51.278a2.033,2.033,0,0,0,4.065,0V22.43a7.879,7.879,0,0,0,0-15.248ZM5.3,18.663A3.855,3.855,0,1,1,9.15,14.806,3.86,3.86,0,0,1,5.3,18.663Z"
+          transform="translate(0 0)"
+        />
+      </G>
+    </G>
+  </Svg>
+);
+
+export default FindOfferComponent;
